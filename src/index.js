696d2d4d0b7d4e4784a5971a42de6aed6d5f8468
Rafael Guimarães <rafaelguimaraeswm@gmail.com>
2019-09-07 15:10:09 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Component} from 'react-native';
+import AppContainer from './navigation';
+
+class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+export default App;
---
+++ src/navigation.js
@@ -0,0 +1,11 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Home from './screens/Home';
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Home,
+  },
+});
+
+export default createAppContainer(AppNavigator);
